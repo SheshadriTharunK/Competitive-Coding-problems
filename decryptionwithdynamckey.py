c120290ddcf49685ccede4cf82a208902171262e
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-04-26 11:37:44 -0700

    Add files via upload

--- decryptionwithdynamckey.py
+++ decryptionwithdynamckey.py
@@ -1,24 +1,10 @@
-s=input()
-n=int(input())
-plain=''
-p=1
-for i in s:
-    if i.isupper():
-        plain+=chr((ord(i)-p-65)%26+65)
-
-       
-    elif i.islower():
-        plain+=chr((ord(i)-p-97)%26+97)
-   
-       
-    else:
-        plain+=i
-    p+=1 
-    if p==n+1:
-            p=1
-    
-    
-    
-    
-    
-print(plain)
+cipher=input().upper()
+secret=input().upper()
+key=secret 
+tmp=''
+res=''
+for i in range(len(cipher)):
+    tmp=chr((ord(cipher[i])-ord(key[i]))%26+65)
+    res+=tmp
+    key+=tmp 
+print(res)
