f69be6d89cf184f1520f7a7eeaabaf888c464dba
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-05-06 22:45:28 -0700

    Add files via upload

---
+++ NumberGroups.py
@@ -0,0 +1,7 @@
+k=int(input())
+print(k**3)
+
+'''The positive odd numbers are sorted in ascending order as 1,3,5,7,9,11,13,15,17,19...... 
+(1),(3,5),(7,9,11),(13,15,17,19),....
+if k=3:
+    7+9+11=27'''
