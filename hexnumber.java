aecaa6dce99b603891d54b0e79d11c95820332d9
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-05-17 02:59:05 -0700

    Add files via upload

---
+++ hexnumber.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+       
+        System.out.println((int)(2*Math.pow(n,2)-n));
+        
+        
+        
+        
+        
+    }
+}
