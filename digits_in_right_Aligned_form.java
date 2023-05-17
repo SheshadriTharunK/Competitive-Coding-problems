c83d86efda1aead21a111d735266fa65d70b0648
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-05-17 00:16:31 -0700

    Add files via upload

---
+++ digits_in_right_Aligned_form.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        System.out.println(String.format("%3d",a));
+        System.out.println(String.format("%3d",b));
+        System.out.println(String.format("%3d",c));
+    }
+}
