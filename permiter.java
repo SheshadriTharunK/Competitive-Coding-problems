5db9ce1196e0362aedc651af38eda87092e91da2
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-05-19 02:13:41 -0700

    Add files via upload

---
+++ permiter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class areal
+{
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+        int a=scanner.nextInt();
+        double b=Math.sqrt(a);
+        double result=4*b;
+        System.out.println(result);
+    }
+}
