345b560546e03cc3021484e17565c642b816427b
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-05-21 06:01:50 -0700

    Add files via upload

---
+++ arealchallenge.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class areal
+{
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+       double a=scanner.nextDouble();
+        double b=Math.sqrt(a);
+        double result=4*b;
+        System.out.println(result);
+    }
+}
