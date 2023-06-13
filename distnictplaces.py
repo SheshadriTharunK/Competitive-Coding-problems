ad288b94ee344d39be78d2a14212cafb69d9a44b
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-06-13 17:10:26 +0530

    Add files via upload

---
+++ distnictplaces.py
@@ -0,0 +1,38 @@
+'''DistinctPlaces
+Given the list of places travelled at different times for vacation/business by Mr.X, find out how many distinct places Mr.X have been visited.
+
+Input Format
+
+The first line contains a number N, number of places travelled.
+
+The next N lines consists of numbers, which are the unique codes of places visited. The codes are just integers.
+
+Constraints
+
+1 <= N <= 2*10^6
+
+Output Format
+
+Output one number, the number of distinct codes.
+
+Sample Input 0
+
+5
+10
+20
+10
+20
+15
+Sample Output 0
+
+3
+Sample Input 1
+
+3
+10
+20
+100
+Sample Output 1
+
+3
+'''
