92dcadfb4a0d8de35d82ebf19684ef6ab4914ec7
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-07-23 00:27:28 +0530

    Update BigIntegerAddition.py

--- BigIntegerAddition.py
+++ BigIntegerAddition.py
@@ -1,3 +1,45 @@
+'''BigIntegerAddition
+
+Given two numbers X and Y, each has N and M decimal digits respectively, you need to compute the digits after adding X and Y. Assume that most significant digit of each number is at left most side.
+
+Input Format
+
+The first line contains N and M.
+
+The second line contains N digits, separated by spaces, for X.
+
+The third line contains M digits, separated by spaces, for Y.
+
+No leading zeros are available in X and Y.
+
+Constraints
+
+1 <= N,M <= 10^4
+
+Each digit is between 0 and 9, inclusive
+
+Output Format
+
+Output a line that consists of the decimal digits representing X + Y. Keep a space between digits.
+
+Sample Input 0
+
+3 3
+1 2 3
+4 5 6
+Sample Output 0
+
+5 7 9
+Sample Input 1
+
+1 1
+4 
+6
+Sample Output 1
+
+1 0
+
+​'''
 n,m=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
