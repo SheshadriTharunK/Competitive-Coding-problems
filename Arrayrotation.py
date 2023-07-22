be28553a98cc1a639fe2d78e6b3498305e306355
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-07-23 00:17:08 +0530

    Update Arrayrotation.py

--- Arrayrotation.py
+++ Arrayrotation.py
@@ -1,3 +1,13 @@
+'''Given an array of integers arr[] of size N and an integer, the task is to rotate the array elements to the left by d positions.
+
+Examples:  
+
+Input: 
+arr[] = {1, 2, 3, 4, 5, 6, 7}, d = 2
+Output: 3 4 5 6 7 1 2
+
+Input: arr[] = {3, 4, 5, 6, 7, 1, 2}, d=2
+Output: 5 6 7 1 2 3 4'''
 n,d=map(int,input().split())
 arr=list(map(int,input().split()))
 arr1=[]
