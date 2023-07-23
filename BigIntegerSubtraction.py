5f49776aabfc922d0f5401b1fd7a1f7dcdba4771
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-07-23 14:57:38 +0530

    BigIntegerSubtraction.py

--- BigIntegerSubtraction.py
+++ BigIntegerSubtraction.py
@@ -1,6 +1,7 @@
-'''BigIntegerAddition
+''''''
+BigIntegerSubtraction
 
-Given two numbers X and Y, each has N and M decimal digits respectively, you need to compute the digits after adding X and Y. Assume that most significant digit of each number is at left most side.
+GGiven two numbers X and Y, each has N and M decimal digits respectively, you need to compute the digits after subtracting Y from X. Assume that most significant digit of each number is at left most side.
 
 Input Format
 
@@ -18,29 +19,73 @@ Constraints
 
 Each digit is between 0 and 9, inclusive
 
+X >= Y and N >= M
+
 Output Format
 
-Output a line that consists of the decimal digits representing X + Y. Keep a space between digits.
+Output a line that consists of the decimal digits representing X - Y. Keep a space between digits.
 
 Sample Input 0
 
 3 3
-1 2 3
 4 5 6
+1 2 3
 Sample Output 0
 
-5 7 9
-Sample Input 1
+3 3 3
+​'''
 
-1 1
-4 
-6
-Sample Output 1
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+sum=''
+i=n-1
+s=n-1
 
-1 0
 
- 
-1
+
+
+
+
+
+if (m==n)and (a[n-1]<b[m-1]):
+    s1=''.join(map(str,a))
+    s2=''.join(map(str,b))
+    sum=str(int(s1)-int(s2))
+elif (m==n):
+    while(i!=0):
+            carry=a[i]-b[i]
+            i-=1
+            sum+=str(carry)
+    carry=a[i]-b[i]
+    sum=str(carry)+sum[::-1]
+
+elif(a[s]==0):
+    s1=''.join(map(str,a))
+    s2=''.join(map(str,b))
+    sum=str(int(s1)-int(s2))
+elif (a[n-1]<b[m-1]):
+    s1=''.join(map(str,a))
+    s2=''.join(map(str,b))
+    sum=str(int(s1)-int(s2))
+
+else:
+    k=m-1
+    for i in range(n-1,-1,-1):
+            if k==-1:
+                carry=a[:n-m]
+                s=''.join(map(str,carry))
+            else:
+                carry=a[i]-b[k]
+                sum+=str(carry)
+                k=k-1
+    sum=str(s)+sum[::-1]
+for i in sum:
+    print(i,end=' ')
+    
+    
+    
+
 ​'''
 
 n,m=map(int,input().split())
