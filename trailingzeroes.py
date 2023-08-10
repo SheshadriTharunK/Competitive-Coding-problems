8cb24d3c3f66d2ce8978292045aac77e4a62e67b
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-08-10 17:48:15 +0530

    trailingzeroes.py

--- trailingzeroes.py
+++ trailingzeroes.py
@@ -1,3 +1,26 @@
+'''Given an integer n, write a function that returns count of trailing zeroes in n!. 
+Examples : 
+
+Input: n = 5
+Output: 1 
+Factorial of 5 is 120 which has one trailing 0.
+
+Input: n = 20
+Output: 4
+Factorial of 20 is 2432902008176640000 which has
+4 trailing zeroes.
+
+Input: n = 100
+Output: 24'''
+
+
+
+
+
+
+
+
+
 
 class Solution:
     def trailingZeroes(self, N):
