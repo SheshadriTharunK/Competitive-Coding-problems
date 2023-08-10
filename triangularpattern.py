2069c6e044ea1b3f0d66251084ed57043395869b
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-08-10 17:19:54 +0530

    Right_triangularpattern.py

--- triangularpattern.py
+++ triangularpattern.py
@@ -1,3 +1,56 @@
+'''Given a number N, print a right triangle consisting of +, |, \, -, and the whitespace character.
+
+Note: use escape character to display \.
+
+Input Format
+
+A single line with one number, N.
+
+Constraints
+
+1 <= N <= 20
+
+Output Format
+
+See sample testcases.
+
+Sample Input 0
+
+5
+Sample Output 0
+
++
+| \
+|   \
+|     \
++-------+
+Sample Input 1
+
+2
+Sample Output 1
+
++
++-+
+Sample Input 2
+
+1
+Sample Output 2
+
++'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 n=int(input())
 if n==1:
     print('+')
