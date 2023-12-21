53d4e42e75815b1b4346240dfe47832fd9b1c5d1
SheshadriTharunK <97800608+SheshadriTharunK@users.noreply.github.com>
2023-12-21 18:38:58 +0530

    Paranthesis_checker.py

---
+++ Paranthesis_checker.py
@@ -0,0 +1,99 @@
+'''Find duplicates in an array
+
+Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array. Return the answer in ascending order. If no such element is found, return list containing [-1]. 
+
+Note: The extra space is only for the array to be returned. Try and perform all operations within the provided array. 
+
+Example 1:
+
+Input:
+N = 4
+a[] = {0,3,1,2}
+Output: 
+-1
+Explanation: 
+There is no repeating element in the array. Therefore output is -1.
+Example 2:
+
+Input:
+N = 5
+a[] = {2,3,1,2,3}
+Output: 
+2 3 
+Explanation: 
+2 and 3 occur more than once in the given array.
+Your Task:
+Complete the function duplicates() which takes array a[] and n as input as parameters and returns a list of elements that occur more than once in the given array in a sorted manner. 
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+
+Constraints:
+1 <= N <= 105
+0 <= A[i] <= N-1, for each valid i'''
+#User function Template for python3
+
+class Solution:
+    
+    def ispar(self,x):
+
+        st=[]
+        for i in x:
+            if i=='{' or i=='[' or i=='(':
+                st.append(i) 
+            else:
+                if i=='}':
+                    if not st or st.pop()!='{':
+                        return False 
+                if i==')':
+                    if not st or st.pop()!='(':
+                        return False 
+                if i==']':
+                    if not st or st.pop()!='[':
+                        return False 
+        return len(st)==0
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        #n = int(input())
+        #n,k = map(int,imput().strip().split())
+        #a = list(map(int,input().strip().split()))
+        s = str(input())
+        obj = Solution()
+        if obj.ispar(s):
+            print("balanced")
+        else:
+            print("not balanced")
+# } Driver Code Ends
+
+
+
+
+
+
+
+
+
